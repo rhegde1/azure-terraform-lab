7ff9867134aa4889a10861b6dea493fe467cef65
Ritesh Hegde <ritesh.hegde@gmail.com>
2024-03-21 01:31:18 +0800

    fix 7

--- versions.tf
+++ versions.tf
@@ -20,8 +20,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = ${{ secrets.ARM_SUBSCRIPTION_ID }}
-  tenant_id       = ${{ secrets.ARM_TENANT_ID }}
-  client_id       = ${{ secrets.ARM_CLIENT_ID }}
-  client_secret   = ${{ secrets.ARM_SECRET_ID }}
+  # subscription_id = ${{ secrets.ARM_SUBSCRIPTION_ID }}
+  # tenant_id       = ${{ secrets.ARM_TENANT_ID }}
+  # client_id       = ${{ secrets.ARM_CLIENT_ID }}
+  # client_secret   = ${{ secrets.ARM_SECRET_ID }}
 }
